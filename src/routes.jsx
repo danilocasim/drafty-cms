70dbed53ea98e8a557e731af7d2fa66450d6b1eb
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-01 16:07:56 +0800

    feat: add routing for addPost and postPage for rendering post using postId parameter

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login/Login.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import Errorpage from "./pages/Errorpage/Errorpage.jsx";
+import AddPostpage from "./pages/AddPostpage/AddPostpage.jsx";
+import Postpage from "./pages/PostPage/Postpage.jsx";
 
 const routes = [
   {
@@ -11,8 +13,10 @@ const routes = [
     errorElement: <Errorpage />,
     children: [
       { index: true, element: <Homepage /> },
+      { path: "/addPost", element: <AddPostpage /> },
       { path: "/login", element: <Login /> },
       { path: "/signup", element: <Signup /> },
+      { path: "/post/:postId", element: <Postpage /> },
     ],
   },
 ];
