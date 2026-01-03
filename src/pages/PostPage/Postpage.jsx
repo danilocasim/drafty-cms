9437da751e6f2bb11a029e954c57bc9e0d4cc4d3
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-03 17:39:11 +0800

    feat: add CRUD operation for comments to make it more dynamic

--- src/pages/PostPage/Postpage.jsx
+++ src/pages/PostPage/Postpage.jsx
@@ -1,21 +1,155 @@
 import { useParams } from "react-router";
 import { useCurrentPost } from "../../hooks/useCurrentPost";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context";
 
 function Postpage() {
   const { postId } = useParams();
-  const { token } = useContext(AuthContext);
+  const { token, user } = useContext(AuthContext);
+  const [editting, setEditting] = useState(null);
+
+  const [updatedComment, setUpdatedComment] = useState(null);
   const [post] = useCurrentPost(
     `http://localhost:8000/blog/v1/post/${postId}`,
     token
   );
 
-  console.log(post);
+  const [comments, setComments] = useState([]);
+  const [content, setContent] = useState("");
+  const [recentComment, setRecentComment] = useState(null);
+
+  function toggleEditComment(id, content) {
+    if (id === editting) return setEditting(null);
+    setEditting(id);
+
+    setUpdatedComment(content);
+  }
+
+  function onChangeContent(e) {
+    setContent(e.target.value);
+  }
+
+  function onChangeUpdateComment(e) {
+    setUpdatedComment(e.target.value);
+  }
+  useEffect(() => {
+    fetch(`http://localhost:8000/blog/v1/post/${postId}/comment`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setComments(data.data);
+        console.log(data.data);
+      });
+  }, [postId, token, recentComment]);
+
+  function addComment() {
+    fetch(`http://localhost:8000/blog/v1/post/${postId}/comment`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({ content: content, userId: user.id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setRecentComment(data.data);
+      });
+  }
+
+  function deleteComment(id) {
+    fetch(`http://localhost:8000/blog/v1/post/${postId}/comment/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      method: "DELETE",
+      mode: "cors",
+      body: JSON.stringify({ content: content, userId: user.id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setRecentComment(data.data);
+      });
+  }
+
+  function editComment(id) {
+    fetch(`http://localhost:8000/blog/v1/post/${postId}/comment/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      method: "PUT",
+      mode: "cors",
+      body: JSON.stringify({ content: updatedComment }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setRecentComment(data.data);
+      });
+
+    setEditting(null);
+  }
+
   return (
     <div>
       {post && <p>{post.title}</p>}
       {post && <div dangerouslySetInnerHTML={{ __html: post.content }}></div>}
+      <br />
+      <br />
+      Comments
+      {comments &&
+        comments.map((comment) => {
+          return (
+            <div key={comment.id}>
+              <p>
+                User: {comment.User.username}{" "}
+                {post.userId == user.id && <i>Author</i>}
+              </p>
+              {!editting && (
+                <div>
+                  <p> {comment.content}</p>
+                </div>
+              )}
+              {editting == comment.id && (
+                <div>
+                  <input
+                    value={updatedComment}
+                    type='text'
+                    name='content'
+                    id='content'
+                    onChange={onChangeUpdateComment}
+                  />
+                  <button onClick={() => editComment(comment.id)}>
+                    Update
+                  </button>
+                </div>
+              )}
+              <button onClick={() => deleteComment(comment.id)}>Delete</button>
+              {comment.userId == user.id && (
+                <button
+                  onClick={() => toggleEditComment(comment.id, comment.content)}
+                >
+                  {comment.id === editting && "Close"}
+                  {!editting && "Edit"}
+                </button>
+              )}
+            </div>
+          );
+        })}
+      <input
+        type='text'
+        name='content'
+        id='content'
+        onChange={onChangeContent}
+      />
+      <button onClick={addComment}>Submit</button>
     </div>
   );
 }
