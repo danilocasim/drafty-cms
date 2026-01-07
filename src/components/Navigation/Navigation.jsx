ac2217e2efedf5bd8c27b5d248f5f353cfe61d28
Danilo Casim Jr <danilocasim174@gmail.com>
2026-01-07 17:06:12 +0800

    fix: add token as dependency for rendering login signup link
    so that if there's no token, no need to render the login signup

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,18 +4,12 @@ import { AuthContext } from "../../context";
 import style from "./Navigation.module.css";
 
 function Navigation() {
-  const { user, logout } = useContext(AuthContext);
+  const { user, token, logout } = useContext(AuthContext);
   return (
     <nav className={style.nav}>
       <h1 className={style.logo}>
         <Link to='/'>Drafty</Link>
       </h1>
-      {!user && (
-        <div className={style.btnWrapper}>
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>Signup</Link>
-        </div>
-      )}
       {user && (
         <div className={style.btnWrapper}>
           <Link to='/addPost'>Add Post</Link>
@@ -25,6 +19,13 @@ function Navigation() {
           </button>
         </div>
       )}
+      {!user ||
+        (!token && (
+          <div className={style.btnWrapper}>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+          </div>
+        ))}
     </nav>
   );
 }
