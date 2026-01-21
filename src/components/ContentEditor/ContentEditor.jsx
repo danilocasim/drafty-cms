7fa155cb73c157099930966418e0c28c618eac0a
Danilo Casim Jr. <danilocasim174@gmail.com>
2026-01-21 14:58:32 +0800

    chore: add prismjs packages for styling of code

--- src/components/ContentEditor/ContentEditor.jsx
+++ src/components/ContentEditor/ContentEditor.jsx
@@ -1,4 +1,4 @@
-import { Editor } from "@tinymce/tinymce-react";
+import { Editor } from '@tinymce/tinymce-react';
 
 function ContentEditor({ editorRef, content }) {
   return (
@@ -10,32 +10,35 @@ function ContentEditor({ editorRef, content }) {
         height: 400,
         menubar: false,
         plugins: [
-          "advlist",
-          "autolink",
-          "lists",
-          "link",
-          "image",
-          "charmap",
-          "preview",
-          "anchor",
-          "searchreplace",
-          "visualblocks",
-          "code",
-          "fullscreen",
-          "insertdatetime",
-          "media",
-          "table",
-          "code",
-          "help",
-          "wordcount",
+          'advlist',
+          'autolink',
+          'lists',
+          'link',
+          'image',
+          'charmap',
+          'preview',
+          'anchor',
+          'searchreplace',
+          'visualblocks',
+          'code',
+          'fullscreen',
+          'insertdatetime',
+          'media',
+          'table',
+          'code',
+          'help',
+          'wordcount',
+          'codesample',
         ],
+        codesample_global_prismjs: true,
+
         toolbar:
-          "undo redo | blocks | " +
-          "bold italic forecolor | alignleft aligncenter " +
-          "alignright alignjustify | bullist numlist outdent indent | " +
-          "removeformat | help",
+          'undo redo | blocks | ' +
+          'bold italic forecolor | alignleft aligncenter ' +
+          'alignright alignjustify | bullist numlist outdent indent | ' +
+          'removeformat | help | codesample',
         content_style:
-          "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
       }}
     />
   );
